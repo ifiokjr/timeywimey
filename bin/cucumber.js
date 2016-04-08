d30afffe00c84015626b9a1081d562f9748b2869
Ifiok Jr <ifiokotung@gmail.com>
2016-04-08 11:31:32 +0100

    chore(versions): update to latest versions and begin docker integration

--- bin/cucumber.js
+++ bin/cucumber.js
@@ -1,20 +1,70 @@
-import { argv } from 'yargs';
+import 'colors';
+
 import { Cli } from 'cucumber';
+import { exec } from 'child_process';
 const cli = Cli(process.argv);
 
-console.log(process.argv.slice(2));
-console.log(argv);
+const runWithDocker: Boolean = process.env.NODE_ENV === 'test:docker';
 
-cli.run((success) => {
-  const code = success ? 0 : 1;
+if (runWithDocker) {
+  // Get a list of available docker machines
+  runDockerMachineCommand('docker-machine ls')
 
-  function exitNow () {
-    console.log(`exiting with code ${code}`);
-  }
+  // Format the output and obtain the default configuration
+  .then((output) => {
+    return formatLS(output)['default'];
+  })
+  // Start the default machine if available otherwise throw an error
+  .then((machine) => {
+    if (!machine) {
+      throw new Error('Please set up a default machine -- We\'re working on fallbacks');
+    }
+  })
+  // Run docker on the default machine
 
-  if (process.stdout.write('')) {
-    exitNow();
-  } else {
-    process.stdout.on('drain', exitNow);
-  }
-});
+  // Check for any errors and log them to the console.
+  .catch((err) => {
+    console.log(`${err}`.red);
+  });
+} else {
+  cli.run((success: Boolean) => {
+    const code: Number = success ? 0 : 1;
+
+    function exitNow () {
+      console.log(`exiting with code ${code}`);
+    }
+
+    if (process.stdout.write('')) {
+      exitNow();
+    } else {
+      process.stdout.on('drain', exitNow);
+    }
+  });
+}
+
+/**
+ * Run a command and make the raw result available
+ */
+
+function runDockerMachineCommand (command: String): Promise {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(stderr);
+        return;
+      }
+
+      resolve(stdout);
+    });
+  });
+}
+
+/**
+ * Return an object containing all the available machines,
+ * their status, driver and url
+ * @param  {[type]} input: String        [description]
+ * @return {[type]}        [description]
+ */
+function formatLS (input: String): object {
+
+}
